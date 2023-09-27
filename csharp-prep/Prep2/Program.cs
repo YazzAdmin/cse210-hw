1945de165bce88dd9e4978dc68990ee30451995d
YazzAdmin <YazzAdmin.u553009672@byui.edu>
2023-09-26 19:52:43 -0700

    completed activity 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+
+      static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask name to the user
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
